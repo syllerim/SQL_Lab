913e0114551c442760816b53658e1e78891f4d4d
Mirellys Arteta Davila <mirellysd@spotify.com>
2025-02-16 12:52:17 +0000

    Generate Billing Account Id - fix

--- 7.Generar_Campo_billing_account_id.sql
+++ 7.Generar_Campo_billing_account_id.sql
@@ -2,12 +2,12 @@ SELECT
   ivd.calls_ivr_id,
   ivd.billing_account_id,
 FROM `keepcoding.ivr_detail` AS ivd
-WHERE 
-  ivd.billing_account_id IS NOT NULL
-  AND ivd.billing_account_id != 'UNKNOWN'
 QUALIFY ROW_NUMBER()
   OVER(
     PARTITION BY CAST(ivd.calls_ivr_id AS STRING) 
-    ORDER BY ivd.module_sequece DESC, ivd.step_sequence DESC
+    ORDER BY 
+      IF(ivd.billing_account_id IS NOT NULL AND ivd.billing_account_id != 'UNKNOWN', 1, 2),
+      ivd.module_sequece DESC, 
+      ivd.step_sequence DESC
   ) = 1
 ORDER BY ivd.calls_ivr_id ASC;
